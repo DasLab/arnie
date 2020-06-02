74e783262c4caeb0d5d6e7399fb3b8850121f41c
Hannah Wayment-Steele <hwayment@DN0a1f8fa9.SUNet>
2020-06-02 14:07:50 -0700

    update RNAGraph

--- RNAGraph/RNAGraph.py
+++ RNAGraph/RNAGraph.py
@@ -41,14 +41,22 @@ class RNAGraph(object):
         self.N = len(self.secstruct)
 
         if colors is not None:
-            color_dct = ['green','blue','purple']
+            #color_dct = ['#3288bd','k','#5e4fa2'] # from Brewer Spectral10
+            #color_dct = ['#003EF5','#ffff33']
+            color_dct =  ['#000000', '#778899']
+            #linewidth_dct = [2.5,1.0]
             self.colors=[color_dct[i] for i in colors]
+            #self.linewidths = [linewidth_dct[i] for i in colors]
         else:
             self.colors=['black']*len(self.secstruct)
+            #self.linewidths = [1.0]*len(self.secstruct)
 
         self.setup_graph()
 
-        self.MLD = nx.algorithms.dag.dag_longest_path_length(self.G)
+        nodes = [n for n in list(self.G.nodes) if not isinstance(n,str)]
+        subgraph = self.G.subgraph(nodes)
+
+        self.MLD = nx.algorithms.dag.dag_longest_path_length(subgraph)
 
         self.n_hairpins, self.n_internal_loops, self.n_3WJs, self.n_4WJs, self.n_5WJs_up = self.count_loops()
 
@@ -85,7 +93,7 @@ class RNAGraph(object):
                         letter='a'
                     elif self.secstruct[i-1]==')':
                         letter='b'
-                    self.G.add_edge('n%d' % i,'h%d%s' % (self.stem_assignment[i-1], letter), length=1, weight=1,color=self.colors[i])
+                    self.G.add_edge('n%d' % i,'h%d%s' % (self.stem_assignment[i-1], letter), len=1, weight=1, color=self.colors[i])
                     # add helix_a node here
 
                 if stem_assignment_right[i] > 0:
@@ -94,21 +102,22 @@ class RNAGraph(object):
                         letter='a'
                     elif self.secstruct[i+1]=='(':
                         letter='b'
-                    self.G.add_edge('n%d' % i,'h%d%s' % (self.stem_assignment[i+1], letter), length=1, weight=1,color=self.colors[i])
+                    self.G.add_edge('n%d' % i,'h%d%s' % (self.stem_assignment[i+1], letter), len=1, weight=1, color=self.colors[i])
                     # add helix_b node here
 
         nuc_nodes = [n for n in list(self.G.nodes) if isinstance(n,str) and n.startswith('n')] # hacky
         for nuc in nuc_nodes:
             ind=int(nuc.replace('n',''))
             if 'n%d' % (ind-1) in nuc_nodes:
-                self.G.add_edge('n%d' % (ind-1), 'n%d' % ind, length=1, weight=1, color=self.colors[ind-1])
+                self.G.add_edge('n%d' % (ind-1), 'n%d' % ind, len=1, weight=1, color=self.colors[ind-1])
                 #print('n%d' % (ind-1), 'n%d' % ind,)
 
 
         for stem_ind in range(1,len(self.stems)+1):
             stem_length = len(self.stems[stem_ind-1])
-            self.G.add_edge('h%da' % (stem_ind),'h%db' % (stem_ind), length=stem_length, weight=stem_length, color='black')
-                #then go through and connect helix modes
+            color_ind = np.where(self.stem_assignment==stem_ind)[0][0]
+            self.G.add_edge('h%da' % (stem_ind),'h%db' % (stem_ind), len=stem_length, weight=stem_length,\
+             color=self.colors[color_ind])
 
         # fix for stems that have no unpaired bases between them
         # dbs = [i for i in range(len(self.secstruct)) if self.secstruct[i:i+2] == ')(']
@@ -120,18 +129,21 @@ class RNAGraph(object):
         for i in range(self.N-1):
 
             if self.stem_assignment[i+1] != self.stem_assignment[i]:
+
                 if self.stem_assignment[i+1] != 0.0 and self.stem_assignment[i] != 0.0:
                     stem_ind_1 = self.stem_assignment[i]
                     stem_ind_2 = self.stem_assignment[i+1]
+                    color_ind = np.where(self.stem_assignment==stem_ind_1)[0][0]
+
                     # print(stem_ind_1, stem_ind_2)
                     # print(self.secstruct[i:i+2])
 
                     if self.secstruct[i:i+2] == '((':
-                        self.G.add_edge('h%da' % stem_ind_1,'h%db' % stem_ind_2, length=1, weight=1,color='black')
+                        self.G.add_edge('h%da' % stem_ind_1,'h%db' % stem_ind_2, len=1, weight=1,color=self.colors[color_ind])
                     elif self.secstruct[i:i+2] == ')(':
-                        self.G.add_edge('h%db' % stem_ind_1,'h%db' % stem_ind_2, length=1, weight=1,color='black')
-                    # elif self.secstruct[i:i+2] == '))':
-                    #     self.G.add_edge('h%db' % stem_ind_1,'h%da' % stem_ind_2, length=1, weight=1,color='black')
+                        self.G.add_edge('h%db' % stem_ind_1,'h%db' % stem_ind_2, len=1, weight=1,color=self.colors[color_ind])
+                    elif self.secstruct[i:i+2] == '))':
+                        self.G.add_edge('h%db' % stem_ind_1,'h%da' % stem_ind_2, length=1, weight=1,color=self.colors[color_ind])
 
 
     def add_edges_r_(self, start_index, end_index, last_helix, last_loop):
@@ -156,7 +168,7 @@ class RNAGraph(object):
                     if last_helix not in list(self.G.nodes):
                         #print(last_helix, len(stems[int(last_helix-1)]))
                         self.G.add_edge(int(last_loop), int(last_helix),
-                                   length = len(self.stems[int(last_helix-1)]),
+                                   len = len(self.stems[int(last_helix-1)]),
                                   weight = len(self.stems[int(last_helix-1)]))
 
                         last_loop = copy(last_helix)
@@ -189,7 +201,7 @@ class RNAGraph(object):
 
         colors = [subgraph[u][v]['color'] for u,v in subgraph.edges()]
 
-        nx.draw(subgraph, pos,node_size=0, edge_color=colors, arrows=False)
+        nx.draw(subgraph, pos, node_size=0, edge_color=colors, arrows=False)
 
     def count_loops(self):
         n_1, n_2, n_3, n_4, n_5 = 0,0,0,0,0
@@ -212,6 +224,7 @@ class RNAGraph(object):
     def get_info(self):
         print("Max ladder distance: %d" % self.MLD)
         print("n_hairpins: %d" % self.n_hairpins)
+        print("n_internal_loops: %d" % self.n_internal_loops)
         print("n_3WJs: %d" % self.n_3WJs)
         print("n_4WJs: %d" % self.n_4WJs)
         print("n_5WJs_up: %d" % self.n_5WJs_up)
