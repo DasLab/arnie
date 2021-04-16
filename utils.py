49f3f2368e0becd6e16944cd32627dca8b74def1
Hannah Wayment-Steele <hwayment@zwetschgen2.local>
2021-04-16 11:43:09 -0700

    update utils.load_package_locations()

--- utils.py
+++ utils.py
@@ -522,6 +522,7 @@ def load_package_locations():
                 return_dct[key] = string
 
     if 'eternafoldparams' not in return_dct.keys():
-      return_dct['eternafoldparams'] = "%s/../params/EternaFoldParams.v1" % return_dct['eternafold']
-      
+      if 'eternafold' in return_dct.keys():                                         
+        return_dct['eternafoldparams'] = "%s/../params/EternaFoldParams.v1" % return_dct['eternafold']
+
     return return_dct
